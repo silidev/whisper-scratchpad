edb99141f2ef7bd2e22f2cf1951d17cee70b40cb
silidev <>
2023-12-23 23:31:34 +0100

    m

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 /* This is NOT my coding style, this is mostly AI generated.*/
 
+class Utils {
+  static insertTextAtCursor = (textarea, addedText) => {
+    const cursorPosition = textarea.selectionStart;
+    const textBeforeCursor = textarea.value.substring(0, cursorPosition);
+    const textAfterCursor = textarea.value.substring(cursorPosition);
+
+    textarea.value = textBeforeCursor + addedText + textAfterCursor;
+    textarea.selectionStart = textarea.selectionEnd = cursorPosition + addedText.length;
+  };
+}
+
 let apiKey = '';
 
 const saveKeyButton = document.getElementById('saveKeyButton');
@@ -99,11 +110,13 @@ const sendAudioToServer = async (audioBlob) => {
     body: formData
   });
   const result = await response.json();
-  whisperResponse.value += " " +
-      !result?.error ? result.text
-          : JSON.stringify(result, null, 2);
+
   if (result.error?.code === "invalid_api_key") {
-    whisperResponse.value = 'You need an API key. Go to <a href="https://platform.openai.com/api-keys">get an API key</a>. If you want to try it out beforehand, you can try it in the ChatGPT Android and iOS apps for free without API key.';
+    whisperResponse.value += 'You need an API key. Go to <a href="https://platform.openai.com/api-keys">get an API key</a>. If you want to try it out beforehand, you can try it in the ChatGPT Android and iOS apps for free without API key.';
+  } else {
+    const addedText = " " +
+        !result?.error ? result.text: JSON.stringify(result, null, 2);
+    Utils.insertTextAtCursor(whisperResponse, addedText);
   }
   navigator.clipboard.writeText(whisperResponse.value).then();
 };
