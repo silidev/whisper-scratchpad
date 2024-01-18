327ec179840deed2df6a989593b17b2bec9c2098
silidev <>
2024-01-18 22:10:29 +0100

    m

--- generated/DontInspect.js
+++ generated/DontInspect.js
@@ -1,3 +1,6 @@
+/*
+This file contains functions that are not meant to be inspected by IntelliJ IDEA, because at least the warnings in this file are not useful, but cannot be disabled.
+ */
 export const sendCtrlZ = () => {
     document.execCommand('undo'); // Yes, deprecated, but works. I will replace it when it fails. Docs: https://developer.mozilla.org/en-US/docs/Web/API/Document/execCommand
 };
