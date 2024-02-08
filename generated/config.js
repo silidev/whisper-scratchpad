26eded4d58dcc9583d33ea05f5448606471a8c56
silidev <>
2024-02-08 17:37:56 +0100

    i

--- generated/config.js
+++ generated/config.js
@@ -3,6 +3,6 @@
 // noinspection SpellCheckingInspection
 export const VERSION = "Saltburn";
 export const INSERT_EDITOR_INTO_PROMPT = true;
-export const NEW_NOTE_DELIMITER = ')))---(((\n';
+export const NEW_NOTE_DELIMITER = ')))---(((';
 export const WHERE_TO_INSERT_AT = "appendAtEnd";
 //# sourceMappingURL=config.js.map
