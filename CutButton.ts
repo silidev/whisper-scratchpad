6767a7f4c7a6a288795186187309673ba430662d
silidev <>
2024-02-12 16:21:27 +0100

    i

--- CutButton.ts
+++ CutButton.ts
@@ -12,24 +12,20 @@ export const createCutFunction =
   return () => {
 
     const currentNote = new CurrentNote(mainEditorTextarea)
-
-    // Because this sometimes (very seldom) does something bad, first backup the whole text to clipboard:
-    clipboard.writeText(mainEditorTextarea.value).then(() => {
-      clipboard.writeText(prefix + currentNote.text().trim() + postfix).then(() => {
-        HtmlUtils.signalClickToUser(buttonWithId("cutButton"))
-        {
-          const DELETE = true
-          if (DELETE) {
-            /* If DELETE==true, the text between the markers is deleted. */
-            currentNote.delete()
-          } else {
-            // When DELETE==false, just select the text between the markers:
-            currentNote.select()
-          }
+    clipboard.writeText(prefix + currentNote.text().trim() + postfix).then(() => {
+      HtmlUtils.signalClickToUser(buttonWithId("cutButton"))
+      {
+        const DELETE = true
+        if (DELETE) {
+          /* If DELETE==true, the text between the markers is deleted. */
+          currentNote.delete()
+        } else {
+          // When DELETE==false, just select the text between the markers:
+          currentNote.select()
         }
-        saveMainEditor()
-        mainEditorTextarea.focus()
-      })
+      }
+      saveMainEditor()
+      mainEditorTextarea.focus()
     })
   }
 }
--- generated/CutButton.js
+++ generated/CutButton.js
@@ -6,24 +6,21 @@ const clipboard = navigator.clipboard;
 export const createCutFunction = (mainEditorTextarea, prefix = "", postfix = "") => {
     return () => {
         const currentNote = new CurrentNote(mainEditorTextarea);
-        // Because this sometimes (very seldom) does something bad, first backup the whole text to clipboard:
-        clipboard.writeText(mainEditorTextarea.value).then(() => {
-            clipboard.writeText(prefix + currentNote.text().trim() + postfix).then(() => {
-                HtmlUtils.signalClickToUser(buttonWithId("cutButton"));
-                {
-                    const DELETE = true;
-                    if (DELETE) {
-                        /* If DELETE==true, the text between the markers is deleted. */
-                        currentNote.delete();
-                    }
-                    else {
-                        // When DELETE==false, just select the text between the markers:
-                        currentNote.select();
-                    }
+        clipboard.writeText(prefix + currentNote.text().trim() + postfix).then(() => {
+            HtmlUtils.signalClickToUser(buttonWithId("cutButton"));
+            {
+                const DELETE = true;
+                if (DELETE) {
+                    /* If DELETE==true, the text between the markers is deleted. */
+                    currentNote.delete();
                 }
-                saveMainEditor();
-                mainEditorTextarea.focus();
-            });
+                else {
+                    // When DELETE==false, just select the text between the markers:
+                    currentNote.select();
+                }
+            }
+            saveMainEditor();
+            mainEditorTextarea.focus();
         });
     };
 };
