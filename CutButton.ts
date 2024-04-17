a00e9b8c2dd551cd9fb75e370817ac91e84dec44
silidev <>
2024-04-17 13:18:43 +0200

    i

--- CutButton.ts
+++ CutButton.ts
@@ -12,21 +12,22 @@ export const createCutFunction =
   return () => {
     mainEditor.Undo.saveState()
     const currentNote = new CurrentNote(mainEditorTextarea)
-    clipboard.writeText(prefix + currentNote.text().trim() + postfix).then(() => {
-      HtmlUtils.signalClickToUser(buttonWithId("cutNoteButton"))
-      {
-        const DELETE = true
-        if (DELETE) {
-          /* If DELETE==true, the text between the markers is deleted. */
-          currentNote.delete()
-        } else {
-          // When DELETE==false, just select the text between the markers:
-          currentNote.select()
-        }
-      }
-      mainEditor.save();
-      alert("The current note was cut out.")
-      mainEditorTextarea.focus()
-    }).catch(Log.error)
+    clipboard.writeText(prefix + currentNote.text().trim() + postfix)
+        .then(() => {
+          HtmlUtils.signalClickToUser(buttonWithId("cutNoteButton"))
+          {
+            const DELETE = true
+            if (DELETE) {
+              /* If DELETE==true, the text between the markers is deleted. */
+              currentNote.delete()
+            } else {
+              // When DELETE==false, just select the text between the markers:
+              currentNote.select()
+            }
+          }
+          mainEditor.save();
+          alert("The current note was cut out.")
+          mainEditorTextarea.focus()
+        }).catch(Log.error)
   }
 }
--- generated/CutButton.js
+++ generated/CutButton.js
@@ -7,7 +7,8 @@ export const createCutFunction = (mainEditorTextarea, prefix = "", postfix = "")
     return () => {
         mainEditor.Undo.saveState();
         const currentNote = new CurrentNote(mainEditorTextarea);
-        clipboard.writeText(prefix + currentNote.text().trim() + postfix).then(() => {
+        clipboard.writeText(prefix + currentNote.text().trim() + postfix)
+            .then(() => {
             HtmlUtils.signalClickToUser(buttonWithId("cutNoteButton"));
             {
                 const DELETE = true;
