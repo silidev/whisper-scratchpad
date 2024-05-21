fdae18946ff3e9e921ab884addc574b854a58832
silidev <>
2024-05-21 17:49:11 +0200

    w

---
+++ generated/CutButton.js
@@ -0,0 +1,31 @@
+import { HtmlUtils } from "./HelgeUtils/HtmlUtils.js";
+import { Log, mainEditor } from "./Main.js";
+import { CurrentNote } from "./CurrentNote.js";
+var buttonWithId = HtmlUtils.NeverNull.buttonWithId;
+var alertAutoDismissing = HtmlUtils.alertAutoDismissing;
+const clipboard = navigator.clipboard;
+export const createCutFunction = (mainEditorTextarea, prefix = "", postfix = "") => {
+    return () => {
+        mainEditor.Undo.saveState();
+        const currentNote = new CurrentNote(mainEditorTextarea);
+        clipboard.writeText(prefix + currentNote.text().trim() + postfix)
+            .then(() => {
+            HtmlUtils.signalClickToUser(buttonWithId("cutNoteButton"));
+            {
+                const DELETE = true;
+                if (DELETE) {
+                    /* If DELETE==true, the text between the markers is deleted. */
+                    currentNote.delete();
+                }
+                else {
+                    // When DELETE==false, just select the text between the markers:
+                    currentNote.select();
+                }
+            }
+            mainEditor.save();
+            alertAutoDismissing("The current note was cut out.");
+            mainEditorTextarea.focus();
+        }).catch(Log.error);
+    };
+};
+//# sourceMappingURL=CutButton.js.map
