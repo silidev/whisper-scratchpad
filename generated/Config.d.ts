003821328c459f399381138223caab66b8a499c3
silidev <>
2024-08-09 15:16:29 +0200

    i

--- generated/Config.d.ts
+++ generated/Config.d.ts
@@ -1,6 +1,6 @@
 import { UiFunctions } from "./Main.js";
 import WhereToPutTranscription = UiFunctions.Buttons.Media.WhereToPutTranscription;
-export declare const VERSION = "Molchi";
+export declare const VERSION = "Fix";
 export declare const WHISPER_TEMPERATURE = "0";
 export declare const INSERT_EDITOR_INTO_PROMPT = true;
 export declare const NEW_NOTE_DELIMITER = ")))---(((\n";
